954a659a38115ba4d35e07f729e4b789f4793e64
ERIC BLAGG, CPA, CMA <33696877+edblagg@users.noreply.github.com>
2021-12-20 16:59:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from ota_update.main.ota_updater import OTAUpdater
 
 
  def download_and_install_update_if_available():
-     o = OTAUpdater('url-to-your-github-project')
-     o.install_update_if_available_after_boot('wifi-ssid', 'wifi-password')
+     o = OTAUpdater('https://github.com/edblagg/hello-world.git')
+     o.install_update_if_available_after_boot('OUTATIME', '2712271227')
 
 
  def start():
