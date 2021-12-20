3a8f2418be2f4e1eb5e485c6d2ee00648cac6a6d
ERIC BLAGG, CPA, CMA <33696877+edblagg@users.noreply.github.com>
2021-12-20 16:52:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-# This program prints Hello, world!
+from ota_update.main.ota_updater import OTAUpdater
 
-print('Hello, world!')
+
+ def download_and_install_update_if_available():
+     o = OTAUpdater('url-to-your-github-project')
+     o.install_update_if_available_after_boot('wifi-ssid', 'wifi-password')
+
+
+ def start():
+     # This program prints Hello, world!
+
+     print('Hello, world!')
+
+
+ def boot():
+     download_and_install_update_if_available()
+     start()
+
+
+ boot()
+
+
