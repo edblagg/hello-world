e6e6417d638aeede44092bf0ada019aa7f4d260b
ERIC BLAGG, CPA, CMA <33696877+edblagg@users.noreply.github.com>
2021-12-21 11:09:58 -0500

    Create main.py

---
+++ main/main.py
@@ -0,0 +1,24 @@
+import machine
+
+from ota_update.main.ota_updater import OTAUpdater
+
+def download_and_install_update_if_available():
+    o = OTAUpdater('https://github.com/edblagg/hello-world.git')
+    o.install_update_if_available_after_boot('OUTATIME', '2712271227')
+
+
+def start():
+     # This program prints Hello, world!
+    print('Hello, world!')
+
+
+def boot():
+    download_and_install_update_if_available()
+    start()
+
+pin=machine.Pin(2, machine.Pin.OUT)
+pin.on()
+pin.off()
+print('Hello, world!')
+
+boot()
